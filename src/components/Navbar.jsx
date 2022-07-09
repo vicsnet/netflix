3ec2e73479b3ab63b2bb8a51f0f0f7b698c3f913
vicsnet <adesanmiadebisi@gmail.com>
2022-07-09 08:26:22 +0100

    vincemovie

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] absolute w-full  ">
+      <h1 className="text-red-600 text-4xl font-bold cursor-point">NETFLIX</h1>
+
+      <div>
+        <button className="pr-4 text-white">Sign In</button>
+        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">
+          Sign Up{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
