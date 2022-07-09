17c009b5690c8fa597545059cf1034832df6b880
vicsnet <adesanmiadebisi@gmail.com>
2022-07-09 09:36:48 +0100

    bg-home

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import request from "../request";
 
 const Main = () => {
-const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const movie = movies[Math.floor(Math.random() * movies.length)];
 
-    
-    
+  useEffect(() => {
+    axios.get(request.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+  console.log(movie);
+
   return (
-    <div>Main</div>
-  )
-}
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.tittle}
+              />
+              <div>
+                  <button className="border bg-gray-300 py-2 px-5">Play</button>
+                  <button className="border  py-2 px-5 ">Watch Later</button>
+              </div>
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
---
+++ src/request.js
@@ -0,0 +1,13 @@
+const key = "7ae4f8fd6f03b00575421f7bec42d21e";
+
+const requests = {
+  requestLatest: `
+https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
+  requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `
+https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
